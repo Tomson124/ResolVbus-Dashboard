509567f8e4ffa7ca8a095050740fa025c6ecdbe1
Thomas Wolfgruber <tomson124@outlook.de>
2018-02-10 20:27:09 +0100

    Adding copyright symbol

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Erstellt von <b><a href="https://tomson124.de" target="_blank">Tomson124</a></b> 2018</span>
+    <span class="created-by">Erstellt von <b><a href="https://tomson124.de" target="_blank">Tomson124</a></b> &copy; 2018</span>
     <div class="socials">
       <a href="https://github.com/Tomson124" target="_blank" class="ion ion-social-github"></a>
       <a href="https://www.facebook.com/TomsonDesign/" target="_blank" class="ion ion-social-facebook"></a>
