c0c2f7fa22ede8426e10ee025d3c1c6c2f06c173
Thomas Wolfgruber <tomson124@outlook.de>
2018-04-23 21:21:26 +0200

    More work

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
 import { appVersion } from './../../app.version';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
+import { ROUTES } from '@app/components/sidebar/sidebar.component';
+
 
 @Component({
   selector: 'app-navbar',
@@ -8,12 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  title = 'VBus-Dashboard';
+  private listTitles: any[];
+  location: Location;
+
   version = 'v' + appVersion;
 
-  constructor() { }
+  constructor(location: Location,  private element: ElementRef) {
+    this.location = location;
+  }
 
   ngOnInit() {
+    this.listTitles = ROUTES.filter(listTitle => listTitle);
+  }
+
+  getTitle() {
+    let titlee = this.location.prepareExternalUrl(this.location.path());
+    if (titlee.charAt(0) === '#') {
+        titlee = titlee.slice( 2 );
+    }
+    titlee = titlee.split('/').pop();
+
+    for (let item = 0; item < this.listTitles.length; item++) {
+        if (this.listTitles[item].path === titlee) {
+            return this.listTitles[item].title;
+        }
+    }
+    return 'Dashboard';
   }
 
 }
