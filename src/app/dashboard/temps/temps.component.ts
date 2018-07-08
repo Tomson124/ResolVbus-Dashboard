63ca42bbec0c07aa34cdbaf8da3bd76911f0b394
Thomas Wolfgruber <tomson124@outlook.de>
2018-07-08 12:35:45 +0200

    Testing

--- src/app/dashboard/temps/temps.component.ts
+++ src/app/dashboard/temps/temps.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { TempsService, Temps } from '../../temps.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -12,8 +12,8 @@ export class TempsComponent implements OnInit {
   @Input() solar: boolean;
   solarNum: number = 0;
   waterNum: number = 1;
-  tempSolar: number;
-  tempWater: number;
+  tempSolar: number = 2;
+  tempWater: number = 10;
   timestamp: string;
 
   temps: Temps;
@@ -22,14 +22,26 @@ export class TempsComponent implements OnInit {
 
   showTemps() {
     this.route.data.subscribe(({ temps }) => {
-      this.temps = temps;
+      //this.temps = temps;
+      this.tempSolar = temps[this.solarNum].rawValue;
+      this.tempWater = temps[this.waterNum].rawValue;
     });
-    this.tempSolar = this.temps[this.solarNum].rawValue;
-    this.tempWater = this.temps[this.waterNum].rawValue;
-    this.timestamp = this.temps[this.solarNum].time;
-  }
+    //this.tempSolar = this.temps[this.solarNum].rawValue;
+    //this.tempWater = this.temps[this.waterNum].rawValue;
+    //this.timestamp = this.temps[this.solarNum].time;
+  }/*
+
+  updateTemps() {
+    this.tempSolar = 5;
+    this.tempService.channel.bind('data-update-event', function(data) {
+      console.log('number: ' + data[0]);
+      this.tempSolar = data[0];
+      this.tempWater = data[1].rawValue;
+    });
+  }*/
 
   ngOnInit() {
-    this.showTemps();
+   // this.showTemps();
+   this.showTemps();
   }
 }
